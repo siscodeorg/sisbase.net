f51d006d9efa94812ef56592571ccdfd67e21110
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-11-18 21:57:23 -0300

    Adds IConfiguration.cs

---
+++ Configuration/IConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisbase.Configuration {
+    /// <summary>
+    /// Interface for creating config files
+    /// </summary>
+    public interface IConfiguration {
+
+    }
+}
