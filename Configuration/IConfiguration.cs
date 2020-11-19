c8a6f69ea57d02fdb802b7a8e8e5e80219998716
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-11-18 22:02:09 -0300

    Implements IConfiguration

--- Configuration/IConfiguration.cs
+++ Configuration/IConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,18 @@ namespace sisbase.Configuration {
     /// Interface for creating config files
     /// </summary>
     public interface IConfiguration {
-
+        /// <summary>
+        /// The path of the config file
+        /// </summary>
+        string Path { get; set; }
+        /// <summary>
+        /// Function that once called will update the config file
+        /// </summary>
+        void Update();
+        /// <summary>
+        /// Function that creates a parses an existing config file from a given file or creates a new one if the file doesn't exists.
+        /// </summary>
+        /// <param name="file"></param>
+        void Create(FileInfo file);
     }
 }
