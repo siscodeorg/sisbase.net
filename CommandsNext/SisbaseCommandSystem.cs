9c206f92dc840dd8f1cdd6562f66fab8a5902a66
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-11-25 12:47:37 -0300

    Start working on SisbaseCommandSystem

--- CommandsNext/SisbaseCommandSystem.cs
+++ CommandsNext/SisbaseCommandSystem.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace sisbase.CommandsNext {
     public class SisbaseCommandSystem {
+        internal PrefixResolver _prefixResolver;
+        internal DiscordSocketClient _client;
+        internal CommandService _commandService = new();
+        internal IServiceProvider _provider;
+        internal ServiceCollection _collection = new();
+
+        public SisbaseCommandSystem(DiscordSocketClient client) => _client = client;
+
+        public async Task InstallCommandsAsync(Assembly assembly) {
+            await _commandService.AddModulesAsync(assembly, _provider);
+        }
     }
 }
