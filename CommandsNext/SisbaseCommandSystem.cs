fe18047a0cc6766ebe77503306dca5901f150ba9
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-11-25 17:50:39 -0300

    Adds GenericPrefixResolver event handler

--- CommandsNext/SisbaseCommandSystem.cs
+++ CommandsNext/SisbaseCommandSystem.cs
@@ -30,7 +30,20 @@ namespace sisbase.CommandsNext {
         }
 
         public async Task InstallCommandsAsync(Assembly assembly) {
+            _client.MessageReceived += GenericPrefixResolver;
             await _commandService.AddModulesAsync(assembly, _provider);
+        }
+
+        private async Task GenericPrefixResolver(SocketMessage message) {
+            if (message is not SocketUserMessage msg) return;
+            var argPos = await _prefixResolver.GetArgumentPositionAsync(msg);
+            if (argPos == 0) return;
+            var ctx = new SocketCommandContext(_client, msg);
+            await _commandService.ExecuteAsync(
+                    context: ctx,
+                    argPos: argPos,
+                    services: _provider
+            );
         }
 
         internal IServiceCollection InitialServiceCollection => _collection
