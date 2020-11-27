1525c41315ca789ce41e6259fe2f2bbd79020252
roridev <t3ctotalmenterandom1@outlook.com>
2020-11-27 20:55:23 -0300

    Implements SisbaseCommandContext

--- CommandsNext/SisbaseCommandContext.cs
+++ CommandsNext/SisbaseCommandContext.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace sisbase.CommandsNext {
-    public class SisbaseCommandContext {
+    public class SisbaseCommandContext : ICommandContext {
+
+        /// <inheritdoc/>
+        public IDiscordClient Client { get; }
+        /// <inheritdoc/>
+        public IGuild Guild { get; }
+        /// <inheritdoc/>
+        public IMessageChannel Channel { get; }
+        /// <inheritdoc/>
+        public IUser User { get; }
+        /// <inheritdoc/>
+        public IUserMessage Message { get; }
+        /// <summary>
+        /// The command being called
+        /// </summary>
+        public CommandInfo Command { get; }
+
+        public SisbaseCommandContext
+           (IDiscordClient client,
+            IUserMessage message,
+            CommandInfo command) {
+            Client = client;
+            Guild = (message.Channel as IGuildChannel)?.Guild;
+            Channel = message.Channel;
+            User = message.Author;
+            Message = message;
+            Command = command;
+        }
     }
 }
