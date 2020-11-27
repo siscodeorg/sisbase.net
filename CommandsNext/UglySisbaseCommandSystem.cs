3cfa81f7fd581a151593f14755c49c9f4e549f17
roridev <t3ctotalmenterandom1@outlook.com>
2020-11-27 20:58:06 -0300

    Adds UglySisbaseCommandSystem.cs

---
+++ CommandsNext/UglySisbaseCommandSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisbase.Workarounds {
+    // Welcome to the uglies.
+    // You shouldn't even be seeing this unless you are code-digging or working on the lib directly.
+    // All the functions stated below are mere internal workarounds that without knowing the inner
+    // workings could break stuff fabulously.
+    // Only use this if you are aware of what it does and why its here.
+    internal static partial class Ugly {
+    }
+}
