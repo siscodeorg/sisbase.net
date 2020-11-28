8318b8322377ed9447b7d4692945444a9eb2137d
roridev <t3ctotalmenterandom1@outlook.com>
2020-11-27 21:21:30 -0300

    [Ugly] Adds ValidateAndGetBestMatch function

--- CommandsNext/UglySisbaseCommandSystem.cs
+++ CommandsNext/UglySisbaseCommandSystem.cs
@@ -16,6 +16,72 @@ namespace sisbase.Workarounds {
     internal static partial class Ugly {
 
         /// <summary>
+        /// Reason for ugly : Feature request via PR on hold.<br></br>
+        /// Dies if Discord.Net#1700 Gets merged.
+        /// </summary>
+        internal static async Task<(IResult, Optional<CommandMatch>)> ValidateAndGetBestMatch(SearchResult matches, ICommandContext context, IServiceProvider provider, MultiMatchHandling multiMatchHandling = MultiMatchHandling.Exception) {
+            if (!matches.IsSuccess)
+                return (matches, Optional.Create<CommandMatch>());
+
+            var commands = matches.Commands;
+            var preconditionResults = new Dictionary<CommandMatch, PreconditionResult>();
+
+            foreach (var command in commands) {
+                preconditionResults[command] = await command.CheckPreconditionsAsync(context, provider);
+            }
+
+            var successfulPreconditions = preconditionResults
+                .Where(x => x.Value.IsSuccess)
+                .ToArray();
+
+            if (successfulPreconditions.Length == 0) {
+                var bestCandidate = preconditionResults
+                   .OrderByDescending(x => x.Key.Command.Priority)
+                   .FirstOrDefault(x => !x.Value.IsSuccess);
+
+                return (bestCandidate.Value, bestCandidate.Key);
+            }
+
+            var parseResults = new Dictionary<CommandMatch, ParseResult>();
+
+            foreach (var pair in successfulPreconditions) {
+                var parseResult = await pair.Key.ParseAsync(context, matches, pair.Value, provider).ConfigureAwait(false);
+
+                if (parseResult.Error == CommandError.MultipleMatches) {
+                    IReadOnlyList<TypeReaderValue> argList, paramList;
+                    switch (multiMatchHandling) {
+                        case MultiMatchHandling.Best:
+                            argList = parseResult.ArgValues.Select(x => x.Values.OrderByDescending(y => y.Score).First()).ToImmutableArray();
+                            paramList = parseResult.ParamValues.Select(x => x.Values.OrderByDescending(y => y.Score).First()).ToImmutableArray();
+                            parseResult = ParseResult.FromSuccess(argList, paramList);
+                            break;
+                    }
+                }
+
+                parseResults[pair.Key] = parseResult;
+            }
+
+            var weightedParseResults = parseResults
+               .OrderByDescending(x => CalculateScore(x.Key, x.Value));
+
+            var successfulParses = weightedParseResults
+                .Where(x => x.Value.IsSuccess)
+                .ToArray();
+
+            if (successfulParses.Length == 0) {
+                var bestMatch = parseResults
+                    .FirstOrDefault(x => !x.Value.IsSuccess);
+
+                return (bestMatch.Value, bestMatch.Key);
+            }
+
+            var chosenOverload = successfulParses[0];
+
+            return (chosenOverload.Value, chosenOverload.Key);
+        }
+
+
+        /// <summary>
         /// Reason for Ugly : Clone of library code and dependency of ValidateAndGetBestMatch. <br></br>
         /// Dies if Discord.Net#1700 Gets merged.
         /// </summary>
