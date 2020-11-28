3cad33ae411f96bd1d105ddcef2facb9e1cec371
roridev <t3ctotalmenterandom1@outlook.com>
2020-11-28 15:30:27 -0300

    Adds ExtensionSisbaseCommandContext.cs

---
+++ CommandsNext/Extensions/ExtensionSisbaseCommandContext.cs
@@ -0,0 +1,11 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisbase.CommandsNext.Extensions {
+    public static class ExtensionSisbaseCommandContext {
+    }
+}
