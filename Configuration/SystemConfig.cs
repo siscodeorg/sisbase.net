c84dc6ac563432451d72738bf8214fb1818c4c48
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-09 18:57:36 -0300

    Implements SystemConfig

--- Configuration/SystemConfig.cs
+++ Configuration/SystemConfig.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace sisbase.Configuration {
-    public class SystemConfig {
+    public class SystemConfig : IConfiguration {
+        [JsonProperty] public Dictionary<string, SystemData> Systems { get; set; }
+        public string Path { get; set; }
+
+        public void Create(FileInfo file) {
+            Path = file.FullName;
+            if(file.Exists) {
+                var data = JsonConvert.DeserializeObject<SystemConfig>(File.ReadAllText(Path));
+                Systems = data.Systems;
+                return;
+            }
+            Update();
+        }
+
+        public void Update() => File.WriteAllText(Path, JsonConvert.SerializeObject(this, Formatting.Indented));
     }
 
     public record SystemData (string Name, bool Enabled);
