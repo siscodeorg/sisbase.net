e6c2e2ac6243522fa836e55fbba9e2a027793417
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-10 12:48:55 -0300

    Adds SystemManager.cs

---
+++ Systems/SystemManager.cs
@@ -0,0 +1,13 @@
+﻿using Discord.WebSocket;
+using sisbase.CommandsNext;
+using sisbase.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisbase.Systems {
+    public class SystemManager {
+    }
+}
