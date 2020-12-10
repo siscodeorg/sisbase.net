18f864eb33d954de05cb3601fa939fae99a9b350
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-10 20:46:28 -0300

    Adds SisbaseResult.cs

---
+++ Common/SisbaseResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisbase.Common {
+    public class SisbaseResult {
+        public bool IsSucess { get; init; }
+        public string Error { get; init; }
+
+        internal SisbaseResult(bool isSucess, string error) {
+            IsSucess = isSucess;
+            Error = error;
+        }
+
+        public static SisbaseResult FromSucess() => new(true, "");
+        public static SisbaseResult FromError(string errorMessage) => new(false, errorMessage);
+    }
+}
