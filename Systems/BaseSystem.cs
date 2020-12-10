25f0272328f168db994215f608fa6ff7a15b3e03
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-10 00:19:43 -0300

    Implements BaseSystem

---
+++ Systems/BaseSystem.cs
@@ -0,0 +1,20 @@
+﻿using sisbase.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisbase.Systems {
+    public abstract class BaseSystem {
+        public string Name;
+        public string Description;
+        public bool Enabled;
+
+        public abstract Task Activate();
+        public abstract Task Deactivate();
+        public virtual async Task<bool> CheckPreconditions() => true;
+
+        internal SystemData ToSystemData () => new(Name, Enabled);
+    }
+}
