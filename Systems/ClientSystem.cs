f8b6cdb84b2555aa0be7de15be6be6ea8ae25898
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-10 00:48:25 -0300

    Adds missing `client` parameter on ClientSystem.ApplyToClient

--- Systems/ClientSystem.cs
+++ Systems/ClientSystem.cs
@@ -8,6 +8,6 @@ using System.Threading.Tasks;
 namespace sisbase.Systems {
     public abstract class ClientSystem : BaseSystem {
         public DiscordSocketClient Client;
-        public virtual async Task ApplyToClient() { }
+        public virtual async Task ApplyToClient(DiscordSocketClient client) { }
     }
 }
