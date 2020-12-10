54946e6614d646aa8fe3893502f9eeb3e7ce91cf
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-10 00:24:20 -0300

    Implements Scheduler expansion

---
+++ Systems/Expansions/Sheduler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisbase.Systems.Expansions {
+    public interface Sheduler {
+        TimeSpan Timeout { get; }
+        Action RunContinuously { get; }
+    }
+}
