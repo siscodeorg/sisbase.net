b0ec5faf3d0a79aa606ad38ced1093ef8f68c0ca
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-21 13:47:45 -0300

    Adds HasExpansion extension method

--- Systems/ExtensionSystemManager.cs
+++ Systems/ExtensionSystemManager.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using sisbase.Systems.Expansions;
+
 namespace sisbase.Systems {
     public static class ExtensionSystemManager {
         public static string GetSisbaseTypeName(this BaseSystem system)
             => $"{system.GetType().Assembly.GetName().Name}::{system.GetType().FullName}";
+
+        public static bool HasExpansion<T>(this BaseSystem system) where T : SystemExpansion
+            => system.Expansions.Any(x => x is T);
+
     }
 }
