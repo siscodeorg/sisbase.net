5d99d0fac0316e76bbd0253ec3d866d85d72c9ed
roridev <t3ctotalmenterandom1@outlook.com>
2021-01-11 16:15:24 -0300

    Adds inital properties for SisbaseBot

--- SisbaseBot.cs
+++ SisbaseBot.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using sisbase.CommandsNext;
+using sisbase.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +9,9 @@ using System.Threading.Tasks;
 
 namespace sisbase {
     public class SisbaseBot {
+        public DiscordSocketClient Client { get; internal init;}
+        public MainConfig Config { get; internal init; }
+        public SisbaseCommandSystem CommandSystem { get; internal init; }
+        internal PrefixResolver PrefixResolver { get; init; }
     }
 }
