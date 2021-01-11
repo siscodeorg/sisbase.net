946ea45e4e43dd32cdb79cc03b26d0fb5471c5d8
roridev <t3ctotalmenterandom1@outlook.com>
2021-01-11 18:19:14 -0300

    Adds initial test bot

--- sisbase.TestBot/Program.cs
+++ sisbase.TestBot/Program.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace sisbase.TestBot {
     class Program {
         static async Task Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var client = new DiscordSocketClient();
+            var bot = new SisbaseBot(client, new FileInfo($"{Directory.GetCurrentDirectory()}/config.json"));
+            await bot.InstallCommandsAsync(typeof(Program).Assembly);
+            await bot.StartAsync();
         }
     }
 }
