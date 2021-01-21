faba4a5c0c732f7e2e0cc4d6b8048353d9e9dbce
roridev <t3ctotalmenterandom1@outlook.com>
2021-01-21 20:49:27 -0300

    Adds UnorderedListEmbed.cs

---
+++ Embeds/UnorderedListEmbed.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+
+using sisbase.Common;
+
+namespace sisbase.Embeds {
+    public static partial class EmbedBase {
+        public static Embed UnorderedListEmbed<T>(string name,
+            List<T> list) {
+            EmbedBuilder embed = new();
+            var description = list.Count == 0 ? "No Data." : string.Join("\n", list.Select(x => x.ToString()));
+            embed.WithFooter($"「sisbase」・ {VersionUtils.GetVersion()}");
+            embed.WithAuthor(name);
+            embed.WithDescription(description);
+            embed.WithColor(Color.LightOrange);
+            return embed.Build();
+        }
+    }
+}
