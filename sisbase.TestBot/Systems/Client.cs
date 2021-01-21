53bfe8f2433848bdc2beb7e6a937fb743686436c
roridev <t3ctotalmenterandom1@outlook.com>
2021-01-20 22:09:11 -0300

    [TestBot] Adds Example Systems

---
+++ sisbase.TestBot/Systems/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord.WebSocket;
+
+using sisbase.Systems;
+
+namespace sisbase.TestBot.Systems {
+    public class Client : ClientSystem {
+        public override async Task Activate() {
+            Name = "Client";
+            Description = "Example ClientSystem";
+            Client.MessageReceived += Received;
+        }
+
+        public override async Task Deactivate() {
+            Client.MessageReceived -= Received;
+        }
+
+        private async Task Received(SocketMessage arg) {
+            if (arg.Content.ToLowerInvariant() == "ping") {
+                await arg.Channel.SendMessageAsync($"{Client.Latency} **ms**");
+            }
+        }
+    }
+}
