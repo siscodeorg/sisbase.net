97d71a2aeb72c6de699976050f5378f191d18dbc
roridev <t3ctotalmenterandom1@outlook.com>
2021-01-21 20:47:44 -0300

    Adds VersionUtils.cs
    Contain Utilities for getting the version number of the currently running sisbase library.

---
+++ Common/VersionUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisbase.Common {
+    public static class VersionUtils {
+        public static string GetVersion() {
+            var location = typeof(SisbaseBot).Assembly.Location;
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(location);
+            return fileVersionInfo.FileVersion;
+        }
+
+        public static string GetFullVersion() {
+            var location = typeof(SisbaseBot).Assembly.Location;
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(location);
+            return fileVersionInfo.ProductVersion;
+        }
+    }
+}
