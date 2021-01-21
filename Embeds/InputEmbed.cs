6a73774b9faa39f227877e9e615ed79b2065c917
roridev <t3ctotalmenterandom1@outlook.com>
2021-01-21 20:48:50 -0300

    Adds InputEmbed.cs

---
+++ Embeds/InputEmbed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+
+using sisbase.Common;
+
+namespace sisbase.Embeds {
+    public static partial class EmbedBase {
+        public static Embed InputEmbed(string query) {
+            EmbedBuilder embed = new();
+            embed.WithFooter($"「sisbase」・ {VersionUtils.GetVersion()}");
+            embed.WithDescription($"Please Type: {query}");
+            embed.WithColor(Color.Teal);
+            return embed.Build();
+        }
+    }
+}
