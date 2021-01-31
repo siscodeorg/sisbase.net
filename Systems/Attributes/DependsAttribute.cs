d2f6d95b32be8dc76f7fda198135826318c976de
roridev <t3ctotalmenterandom1@outlook.com>
2021-01-31 20:59:41 -0300

    Adds AttributeUsage to DependsAttribute

--- Systems/Attributes/DependsAttribute.cs
+++ Systems/Attributes/DependsAttribute.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace sisbase.Systems.Attributes {
-    public class DependsAttribute {
+    [AttributeUsage(AttributeTargets.Class)]
+    public class DependsAttribute : Attribute {
     }
 }
