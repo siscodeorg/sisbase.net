3fc17a1a4b2b928e8a0b542aa71efc4ee6b15966
roridev <t3ctotalmenterandom1@outlook.com>
2021-02-01 17:16:55 -0300

    Adds ReflectionUtils.cs

---
+++ Common/ReflectionUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord.Commands;
+
+namespace sisbase.Common {
+    internal static class ReflectionUtils {
+
+        private static readonly TypeInfo ObjectTypeInfo = typeof(object).GetTypeInfo();
+
+        internal static PropertyInfo[] GetProperties(TypeInfo ownerType) {
+            var result = new List<PropertyInfo>();
+            while (ownerType != ObjectTypeInfo) {
+                foreach (var prop in ownerType.DeclaredProperties) {
+                    if (prop.SetMethod?.IsStatic == false &&
+                        prop.SetMethod?.IsPublic == true &&
+                        prop.GetCustomAttribute<DontInjectAttribute>() == null)
+                        result.Add(prop);
+                }
+                ownerType = ownerType.BaseType.GetTypeInfo();
+            }
+            return result.ToArray();
+        }
+    }
+}
