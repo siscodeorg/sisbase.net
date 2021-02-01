be334cecb2ffb7c2e75bc6bcc4c168ed39d8b203
roridev <t3ctotalmenterandom1@outlook.com>
2021-01-31 21:06:04 -0300

    Implements DependsAttribute

--- Systems/Attributes/DependsAttribute.cs
+++ Systems/Attributes/DependsAttribute.cs
@@ -7,5 +7,16 @@ using System.Threading.Tasks;
 namespace sisbase.Systems.Attributes {
     [AttributeUsage(AttributeTargets.Class)]
     public class DependsAttribute : Attribute {
+        internal List<Type> Systems = new();
+        public DependsAttribute(params Type[] systems) {
+            foreach(var system in systems) {
+                var result = SystemManager.IsValidType(system);
+                if(result.Any(x => !x.IsSucess)) {
+                    throw new InvalidCastException($"Type {system} isn't a valid system type. Reasons: \n" +
+                        $"{string.Join("\n",result.Select(x => x.Error))}");
+                }
+                Systems.Add(system);
+            }
+        }
     }
 }
