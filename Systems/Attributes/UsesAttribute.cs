bb72f1dae6d24ff5b18bd895880d5049c4e53124
roridev <t3ctotalmenterandom1@outlook.com>
2021-02-04 00:02:32 -0300

    Adds UsesAttribute

---
+++ Systems/Attributes/UsesAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisbase.Systems.Attributes {
+    [AttributeUsage(AttributeTargets.Class)]
+    public class UsesAttribute : Attribute {
+        internal List<Type> types;
+        public UsesAttribute(params Type[] Types)
+            => types = Types.ToList();
+    }
+}
