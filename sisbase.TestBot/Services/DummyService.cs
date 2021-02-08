7705bbe0249cddb866f6ec37f0062c2b6bf42b9b
roridev <t3ctotalmenterandom1@outlook.com>
2021-02-08 14:08:27 -0300

    [TestBot] Adds example code for UseAttribute

---
+++ sisbase.TestBot/Services/DummyService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisbase.TestBot.Services {
+    public class DummyService {
+        public int GetDummyValue() {
+            return 12;
+        }
+    }
+}
