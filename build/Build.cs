429b6a1d1fbe86227119171d0dcd1af95f3019be
roridev <t3ctotalmenterandom1@outlook.com>
2021-02-13 15:14:11 -0300

    Implements Restore Target

--- build/Build.cs
+++ build/Build.cs
@@ -6,10 +6,12 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
 partial class Build : NukeBuild
@@ -40,6 +42,9 @@ partial class Build : NukeBuild
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(_ => _
+                .SetProjectFile(Solution)
+            );
         });
 
     Target Compile => _ => _
