05d9be5871c01b0a0f5432d08f6916ff48465db9
roridev <t3ctotalmenterandom1@outlook.com>
2021-02-14 13:40:00 -0300

    Adds BranchInfo.cs

---
+++ build/Data/Git/BranchInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Data.Git.Enums;
+
+namespace Data.Git {
+    public class BranchInfo {
+        public string Name { get; set; }
+        public string Tag { get; set; }
+        public int IssueID { get; set; }
+        public BranchKind Kind { get; set; }
+    }
+}
