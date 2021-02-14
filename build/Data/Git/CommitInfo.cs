69dfdd85ef7e6fc889d6825fa97a43797cfc7b76
roridev <t3ctotalmenterandom1@outlook.com>
2021-02-14 13:36:58 -0300

    Implements CommitInfo

--- build/Data/Git/CommitInfo.cs
+++ build/Data/Git/CommitInfo.cs
@@ -4,7 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Data.Git.Enums;
+
 namespace Data.Git {
     public class CommitInfo {
+        public string Branch { get; set; }
+        public int PullRequestID { get; set; }
+        public CommitKind Kind { get; set; }
     }
 }
