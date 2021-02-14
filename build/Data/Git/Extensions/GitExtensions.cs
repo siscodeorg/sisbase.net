e55f4b330d213f8dd3126b3274c1454cba43e189
roridev <t3ctotalmenterandom1@outlook.com>
2021-02-14 13:54:04 -0300

    Adds IsFixBranch function

--- build/Data/Git/Extensions/GitExtensions.cs
+++ build/Data/Git/Extensions/GitExtensions.cs
@@ -8,5 +8,7 @@ namespace Data.Git.Extensions {
     public static class GitExtensions {
         public static bool IsFeatureBranch(this string BranchName)
             => BranchName.Contains($"feature/");
+        public static bool IsFixBranch(this string BranchName)
+            => BranchName.Contains("fix/") || BranchName.Contains("bug/");
     }
 }
