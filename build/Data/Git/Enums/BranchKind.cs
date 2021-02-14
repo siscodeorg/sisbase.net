7f9716780092d403943b1b7cce8e882c9a8caddc
roridev <t3ctotalmenterandom1@outlook.com>
2021-02-14 13:38:25 -0300

    Adds BranchKind.cs

---
+++ build/Data/Git/Enums/BranchKind.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Git.Enums {
+    public enum BranchKind {
+        FIX, ISSUE_FIX, FEATURE, CUSTOM_TAG, UNCATEGORIZED
+    }
+}
