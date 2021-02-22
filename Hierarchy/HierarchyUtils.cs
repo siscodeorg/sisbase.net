cac2d59be554a00541740d1635aea71d04807ea8
roridev <t3ctotalmenterandom1@outlook.com>
2021-02-22 20:32:58 -0300

    Adds GetHighestRole function

--- Hierarchy/HierarchyUtils.cs
+++ Hierarchy/HierarchyUtils.cs
@@ -1,7 +1,15 @@
 ﻿// unset
 
+using System.Linq;
+
+using Discord;
+using Discord.WebSocket;
+
 namespace sisbase.Hierarchy {
-    public class HierarchyUtils {
+    public static class HierarchyUtils {
+        public static IRole GetHighestRole(SocketGuildUser user)
+            => user.Roles.OrderByDescending(x => x.Position).FirstOrDefault();
+
         
     }
 }
