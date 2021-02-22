52e1297a63d4a43660cf7b6ea8173d9bbb915a85
roridev <t3ctotalmenterandom1@outlook.com>
2021-02-22 20:34:45 -0300

    Adds IsAbove functions

--- Hierarchy/HierarchyUtils.cs
+++ Hierarchy/HierarchyUtils.cs
@@ -9,7 +9,14 @@ namespace sisbase.Hierarchy {
     public static class HierarchyUtils {
         public static IRole GetHighestRole(SocketGuildUser user)
             => user.Roles.OrderByDescending(x => x.Position).FirstOrDefault();
+       
+        public static bool IsAbove(this IRole left, IRole right)
+            => left.Position > right.Position;
 
-        
+        public static bool IsAbove(this SocketGuildUser user, IRole role)
+            => user.Hierarchy > role.Position;
+
+        public static bool IsAbove(this SocketGuildUser left, SocketGuildUser right)
+            => left.Hierarchy > right.Hierarchy;
     }
 }
