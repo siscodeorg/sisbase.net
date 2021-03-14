6254499d1fb1c77bb0dceb114b3f36b9c4541ac5
roridev <t3ctotalmenterandom1@outlook.com>
2021-03-14 20:38:50 -0300

    Adds MessageStreams.cs

---
+++ Streams/MessageStreams.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Discord;
+using Discord.WebSocket;
+
+namespace sisbase.Streams {
+    public static class MessageStreams {
+        
+    }
+}
