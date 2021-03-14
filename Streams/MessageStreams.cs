7bde358c201b6955f0330f0861415d68d6fe07e2
roridev <t3ctotalmenterandom1@outlook.com>
2021-03-14 20:39:39 -0300

    Implements StreamMessageAsync

--- Streams/MessageStreams.cs
+++ Streams/MessageStreams.cs
@@ -6,6 +6,20 @@ using Discord.WebSocket;
 
 namespace sisbase.Streams {
     public static class MessageStreams {
+        public static async IAsyncEnumerable<IMessage> StreamMessagesAsync (this SocketTextChannel channel) {
+            await foreach(var group in channel.GetMessagesAsync()) {
+                foreach(var elem in group) {
+                    yield return elem;
+                }
+            }
+        }
         
+        public static async IAsyncEnumerable<IMessage> StreamMessagesAsync (this SocketTextChannel channel, IMessage from) {
+            await foreach(var group in channel.GetMessagesAsync(fromMessage:from,dir:Direction.Before)) {
+                foreach(var elem in group) {
+                    yield return elem;
+                }
+            }
+        }
     }
 }
