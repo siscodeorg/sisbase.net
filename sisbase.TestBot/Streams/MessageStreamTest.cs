78544238a040c4c93db51986a6561ae894f8a5f4
roridev <t3ctotalmenterandom1@outlook.com>
2021-03-14 20:41:48 -0300

    [TestBot] Adds example on how to use MessageStreams

---
+++ sisbase.TestBot/Streams/MessageStreamTest.cs
@@ -0,0 +1,45 @@
+﻿// unset
+
+using System;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+using sisbase.CommandsNext;
+using sisbase.Streams;
+
+namespace sisbase.TestBot.Streams {
+    public class MessageStreamTest : ModuleBase<SisbaseCommandContext> {
+        int count;
+        
+        [Command("mstream")]
+        public async Task mstream() {
+            count = 0;
+            var ch = Context.Channel as SocketTextChannel;
+            var edit = await ReplyAsync("Waiting...");
+            IMessage msg = null;
+            
+            await foreach (var message in ch.StreamAllMessagesAsync()) {
+                msg = message;
+                count += 1;
+            }
+
+            await ReplyAsync(embed: getMessageDetails(msg));
+        }
+
+        private Embed getMessageDetails(IMessage message) {
+            EmbedBuilder builder = new();
+            
+            builder
+                .WithDescription($"Author : {message.Author.Mention}\n" +
+                                 $"Content : {message.Content}\n" +
+                                 $"Link : {message.GetJumpUrl()}")
+                .WithFooter($"Timestamp : {message.Timestamp} | Count : {count}")
+                .WithAuthor("Testing MessageStream");
+
+            return builder.Build();
+        }
+    }
+}
