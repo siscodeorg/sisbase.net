d8a6ad1fa7cbfd1501af71691924255ff12eedc2
roridev <t3ctotalmenterandom1@outlook.com>
2021-04-14 01:31:38 -0300

    Adds MessageUtils.cs

---
+++ Common/MessageUtils.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using Discord;
+
+namespace sisbase.Common {
+    public static class MessageUtils {
+    }
+}
