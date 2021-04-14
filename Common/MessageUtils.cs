b1337ed5b19a9d6e0f599fda3c997c0120cede5d
roridev <t3ctotalmenterandom1@outlook.com>
2021-04-14 01:32:11 -0300

    Implements GetNumbers function

--- Common/MessageUtils.cs
+++ Common/MessageUtils.cs
@@ -6,5 +6,10 @@ using Discord;
 
 namespace sisbase.Common {
     public static class MessageUtils {
+        public static List<int> GetNumbers(this IMessage message) {
+            Regex regex = new(@"\d+");
+            var matches = regex.Matches(message.Content);
+            return !matches.Any() ? new List<int>() : matches.Select(x => int.Parse(x.Value)).ToList();
+        }
     }
 }
