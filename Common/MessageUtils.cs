6c18eb82b49aa7af1f50d3fa66747a88b4c3b678
roridev <t3ctotalmenterandom1@outlook.com>
2021-04-14 13:43:22 -0300

    Implements GetEmotes function

--- Common/MessageUtils.cs
+++ Common/MessageUtils.cs
@@ -11,5 +11,11 @@ namespace sisbase.Common {
             var matches = regex.Matches(message.Content);
             return !matches.Any() ? new List<int>() : matches.Select(x => int.Parse(x.Value)).ToList();
         }
+
+        public static List<Emote> GetEmotes(this IMessage message) {
+            Regex regex = new(@"\<\:[^><\s]+\:\d+\>");
+            var matches = regex.Matches(message.Content);
+            return !matches.Any() ? new List<Emote>() : matches.Select(x => Emote.Parse(x.Value)).ToList();
+        }
     }
 }
