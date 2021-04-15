0559abe05ddd43339376c5f89aab6f847759505c
roridev <t3ctotalmenterandom1@outlook.com>
2021-04-14 22:42:11 -0300

    Implements GetResource function

--- Common/ReflectionUtils.cs
+++ Common/ReflectionUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -25,5 +26,8 @@ namespace sisbase.Common {
             }
             return result.ToArray();
         }
+        
+        internal static Stream GetResource(string resourceName)
+            => typeof(SisbaseBot).Assembly.GetManifestResourceStream(resourceName);
     }
 }
