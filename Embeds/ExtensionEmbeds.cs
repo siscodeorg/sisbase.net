b1d8e680645d2a332196cfd11a36818dc5a1bf89
roridev <t3ctotalmenterandom1@outlook.com>
2021-05-21 16:31:21 -0300

    Adds ToEmbedBuilder function

--- Embeds/ExtensionEmbeds.cs
+++ Embeds/ExtensionEmbeds.cs
@@ -11,5 +11,22 @@ namespace sisbase.Embeds {
 
         public static EmbedFooterBuilder ToEmbedFooterBuilder(this EmbedFooter footer) 
             => new() {IconUrl = footer.IconUrl, Text =  footer.Text};
+
+        public static EmbedBuilder ToEmbedBuilder(this Embed embed) {
+            EmbedBuilder dnetBuilder = new();
+            var dnetFields = embed.Fields.Select(x => x.ToEmbedFieldBuilder());
+            dnetBuilder = dnetBuilder
+                .WithTitle(embed.Title)
+                .WithDescription(embed.Description)
+                .WithUrl(embed.Url)
+                .WithFields(dnetFields);
+
+            if (embed.Color.HasValue) dnetBuilder = dnetBuilder.WithColor(embed.Color.Value);
+            if (embed.Timestamp.HasValue) dnetBuilder = dnetBuilder.WithTimestamp(embed.Timestamp.Value);
+            if (embed.Footer.HasValue) dnetBuilder = dnetBuilder.WithFooter(embed.Footer.Value.ToEmbedFooterBuilder());
+            if (embed.Thumbnail.HasValue) dnetBuilder = dnetBuilder.WithThumbnailUrl(embed.Thumbnail.Value.Url);
+
+            return dnetBuilder;
+        }
     }
 }
