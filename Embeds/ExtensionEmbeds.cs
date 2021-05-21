95db7fba3ec8a3dbcd38cd3b2e9fb42180da38ee
roridev <t3ctotalmenterandom1@outlook.com>
2021-05-21 16:30:32 -0300

    Adds ToEmbedFieldBuilder function

--- Embeds/ExtensionEmbeds.cs
+++ Embeds/ExtensionEmbeds.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Linq;
 
 using Discord;
 
 namespace sisbase.Embeds {
     public static class ExtensionEmbeds {
 
+        public static EmbedFieldBuilder ToEmbedFieldBuilder(this EmbedField field) 
+            => new() {Name = field.Name, Value = field.Value, IsInline = field.Inline};
     }
 }
