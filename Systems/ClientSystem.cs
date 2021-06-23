de60082462b3b01d767c95e0bdb53cef040dbeb1
roridev <t3ctotalmenterandom1@outlook.com>
2021-06-23 17:39:07 -0300

    Changes ClientSystem to use BaseSocketClient

--- Systems/ClientSystem.cs
+++ Systems/ClientSystem.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace sisbase.Systems {
     public abstract class ClientSystem : BaseSystem {
-        public DiscordSocketClient Client;
-        public virtual async Task ApplyToClient(DiscordSocketClient client) { }
+        public BaseSocketClient Client;
+        public virtual async Task ApplyToClient(BaseSocketClient client) { }
     }
 }
